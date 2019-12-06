4a93ea108909ec4d790764163170d893695ba614
RookieWeirdo <2053221296@qq.com>
2019-12-06 15:23:49 +0800

    部署配置优化

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['49.235.34.97']
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
